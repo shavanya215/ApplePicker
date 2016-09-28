db618ca70407d6c2085e43f7ef053b13af93abff
shavanya215 <shavanya.215@gmail.com>
2016-09-27 21:35:55 -0400

    apple picker & basket

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -10,6 +10,17 @@ public class Basket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        // Get the current screen position of the mouse from Input 
+        Vector3 mousePos2D = Input.mousePosition;
+        //The Camera's z position sets how far to push the mouse into 3D
+        mousePos2D.z = -Camera.main.transform.position.z;
+        //Convet the point from 2D screen space into 3D game world space 
+        Vector3 mousepos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        //Move the postition of this Basket to the x postion of the Mouse 
+        Vector3 pos = this.transform.position;
+        pos.x = mousepos3D.x;
+        this.transform.position = pos;
+
+
+    }
 }
